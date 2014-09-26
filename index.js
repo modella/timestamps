c4b4a22f8705f8c9d854a91fe7a7816d3ae0e6c7
paulbdavis <paul@dangersalad.com>
2014-09-26 13:18:16 -0400

    parse date strings on instantiation

--- index.js
+++ index.js
@@ -26,4 +26,13 @@ function plugin(createdAtField, updatedAtField, Model) {
 
     if(done) done();
   });
+
+  Model.on('initializing', function(instance, attrs) {
+    if ('string' === typeof attrs[createdAtField]) {
+      attrs[createdAtField] = new Date(attrs[createdAtField]);
+    }
+    if ('string' === typeof attrs[updatedAtField]) {
+      attrs[updatedAtField] = new Date(attrs[updatedAtField]);
+    }
+  });
 }
